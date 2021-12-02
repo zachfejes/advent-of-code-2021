0a1e98c6789ac7c0ce556d195bc0fb56830213ca
toinnovationlab <tolab.convo@gmail.com>
2021-12-01 23:10:51 -0500

    Initialized project and test project

---
+++ AoC.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace AoC.Test {
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AoC/Program.cs
@@ -0,0 +1,10 @@
+﻿
+namespace AoC {
+
+    public class Program {
+
+        public void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
